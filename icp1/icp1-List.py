b2c5863a30ae84c06c0c3b26993460ab07c75998
praveenpoluri <42703022+praveenpoluri@users.noreply.github.com>
2019-08-24 19:31:14 -0500

    icp1

---
+++ icp1/icp1-List.py
@@ -0,0 +1,24 @@
+print('enter the string')
+str1=input()
+str2=str1[2:]
+str3=str2[::-1]
+print(str3)
+
+
+
+
+print('enter integer1')
+a=input()
+j=int(a)
+print('enter integer2')
+b=input()
+d=int(b)
+c=j+d
+k=j-d
+
+e=j*d
+f=j/d
+print(c)
+print(e)
+print(f)
+print(k)
---
+++ icp1/icp2-replace python.py
@@ -0,0 +1,5 @@
+print('enter a sentence')
+a=input()
+print(a)
+b = a.replace("python", "pythons")
+print(b)
